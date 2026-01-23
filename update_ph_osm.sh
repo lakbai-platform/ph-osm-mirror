b4035c71333f778fcc1aec6abc9d11d582622efa
Johann Buere <johannbuere@gmail.com>
2026-01-23 11:38:12 +0800

    Refactor update script to remove data directory dependency and simplify file handling

--- update_ph_osm.sh
+++ update_ph_osm.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 set -e
 
-# Directory where you store data
-DATA_DIR="data"
-mkdir -p "$DATA_DIR"
-
 BASE_URL="https://download.geofabrik.de/asia"
 MD5_URL="$BASE_URL/philippines-latest.osm.pbf.md5"
 PBF_URL="$BASE_URL/philippines-latest.osm.pbf"
 
-LOCAL_MD5_FILE="$DATA_DIR/philippines-latest.osm.pbf.md5"
-LOCAL_PBF_FILE="$DATA_DIR/philippines-latest.osm.pbf"
+LOCAL_MD5_FILE="philippines-latest.osm.pbf.md5"
+LOCAL_PBF_FILE="philippines-latest.osm.pbf"
 
 echo "[INFO] Checking for updated OSM data..."
 
 # Download remote md5
-curl -s -L "$MD5_URL" -o "$DATA_DIR/remote.md5"
+curl -s -L "$MD5_URL" -o "remote.md5"
 
 # Compare with local md5
 if [[ -f "$LOCAL_MD5_FILE" ]]; then
-    if cmp -s "$DATA_DIR/remote.md5" "$LOCAL_MD5_FILE"; then
+    if cmp -s "remote.md5" "$LOCAL_MD5_FILE"; then
         echo "[INFO] No update detected. Data is current."
-        rm "$DATA_DIR/remote.md5"
+        rm "remote.md5"
         exit 0
     fi
 fi
@@ -32,6 +28,6 @@ echo "[INFO] Update detected. Downloading new .osm.pbf..."
 curl -L "$PBF_URL" -o "$LOCAL_PBF_FILE"
 
 # Move new md5 into place
-mv "$DATA_DIR/remote.md5" "$LOCAL_MD5_FILE"
+mv "remote.md5" "$LOCAL_MD5_FILE"
 
 echo "[INFO] Update complete."
